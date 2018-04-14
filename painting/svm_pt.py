15eb9862ab6c89fc960a124ef67d80a075b75bb9
cyj5230 <cyjsherlocked@gmail.com>
2018-04-14 13:27:21 +0800

    painting wrml

--- painting/svm_pt.py
+++ painting/svm_pt.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 import cv2
 
 # import python files
+import vrml
 
 # Basic configuration of pygame
 black = 0, 0, 0
@@ -31,13 +32,15 @@ imgsvm_rw = crw.rwimg(rw_img)
 max_plane = 4
 ttt = [imgsvm_rw] * max_plane
 current_plane = 0
+model_control = True
+world = []
 
 # real world information
 true_point = [\
     [[-2, 0, 0], [-2, 5, 0], [20, 0, 0]], # way
     [[-2, 0, 5], [-2, 0, 0], [20, 0, 5]], # left lamps
     [[-2, 5, 5], [-2, 5, 0], [20, 5, 5]], # right lamps
-    [[10, 25, 17], [10, 25, 2], [30, 0, 17]], # house
+    [[10, 25, 17], [10, 25, 2], [30, 0, 17]] # house
     #[[5, 0, -2], [5, 2.5, -2], [5, 0, -1]] # front wall
 ]
 
@@ -46,7 +49,7 @@ print('Please choose points on two parallel lines.')
 print('Firstly, click on the left point of first line on stairs')
 
 # main loop
-while True:
+while model_control:
     if procsvm_rw.mode == 'i':
         procsvm_rw.mode = 'in_plane'
     screen.fill(black)
@@ -81,6 +84,7 @@ while True:
         pygame.draw.line(screen, red, procsvm_rw.clk_pt[2], procsvm_rw.clk_pt[3], line_width)
         pygame.draw.line(screen, red, procsvm_rw.clk_pt[0], ttt[current_plane].info_2d.vp, line_width)
         pygame.draw.line(screen, red, procsvm_rw.clk_pt[1], ttt[current_plane].info_2d.vp, line_width)
+
     for i in range(procsvm_rw.cc_num):
         draw_x = int(procsvm_rw.clk_pt[i][0])
         draw_y = int(procsvm_rw.clk_pt[i][1])
@@ -91,7 +95,33 @@ while True:
         texture_name = 'texture' + str(current_plane + 1) + '.jpg'
         ttt[current_plane].extract_texture(procsvm_rw, texture_name)
         procsvm_rw.mode = 'f'
-    
+        if current_plane == 3 and model_control:
+            procsvm_rw.mode = 'model'
+            for i in range(max_plane):
+                world.append(ttt[i].info_3d.pos_3d)
+
+    # modeling
+    if procsvm_rw.mode == 'model':
+        vr_pt = vrml.Vrml()
+        faces = []
+        texture = []
+        for i in range(2):
+            texture.append([i, 0])
+            texture.append([i, 1])
+        print(world)
+        faces.append([world[0][3], world[0][2], world[0][0], world[0][1]])
+        faces.append([world[1][3], world[1][2], world[1][0], world[1][1]])
+        faces.append([world[2][0], world[2][2], world[2][3], world[2][1]])
+        faces.append([world[3][0], world[3][2], world[3][3], world[3][1]])
+
+        vr_pt.startVrml()
+        vr_pt.appendPolygon('texture1.jpg', faces[0], texture)
+        vr_pt.appendPolygon('texture2.jpg', faces[1], texture)
+        vr_pt.appendPolygon('texture3.jpg', faces[2], texture)
+        vr_pt.appendPolygon('texture4.jpg', faces[3], texture)
+        vr_pt.endVrml()
+        print("model done")
+        model_control = False
 
 
     # update display
---
+++ painting/vrml.py
@@ -0,0 +1,107 @@
+class Vrml():
+    vrml_file = ""
+    header = ""
+    initial_position = ""
+
+    def __init__(self):
+        self.header = ("#VRML V2.0 utf8\n"
+                  "PointLight{ intensity 1 location 50 100 300 }\n"
+                  "PointLight{ intensity 1 location 40 100 250 }\n"
+                  "PointLight{ intensity 1 location 60 100 250 }\n"
+                  "PointLight{ intensity 1 location 20 100 100 }\n"
+                  # "PointLight{ intensity 3 location 80 100 100 }\n"
+                  # "PointLight{ intensity 5 location 50 400 50 }\n"
+                       )
+                  # "ShapeHints { \n"
+                  # " vertexOrdering  COUNTERCLOCKWISE \n"
+                  # " shapeType       SOLID \n"
+                  # " }\n"
+        # self.initial_position = ("Separator { \n"
+        #                     "Transform {      translation -15 -30 -300}\n"
+        #                     "Transform {      rotation 0 1 0 -1.27   }\n"
+        #                     "Transform {      rotation 1 0 0 0  } \n"
+        #                     "Transform {      rotation 0 0 1 -0.45  }\n")
+    def startVrml(self):
+        # self.vrml_file = self.header + self.initial_position
+        self.vrml_file = self.header + self.initial_position
+
+    def endVrml(self):
+        with open('polygons.wrl', 'w+') as f:
+            f.write(self.vrml_file)
+
+
+    def appendPolygon(self, filename, world_pts, texture_pts):
+        ground_counter = 0
+
+        shape = "Shape {\n"
+        coordinate3 = "coord Coordinate { point [\n"
+        for i in range(len(world_pts)):
+            coordinate3 += "\t"
+            coordinate3 += str(world_pts[i][0])
+            coordinate3 += " "
+            coordinate3 += str(world_pts[i][1])
+            coordinate3 += " "
+            coordinate3 += str(world_pts[i][2])
+            coordinate3 += " ,\n"
+
+            if world_pts[i][2] == 0:
+                ground_counter += 1
+
+        coordinate3 += "]}\n"
+
+        texture = "appearance Appearance { texture ImageTexture { url \"" + filename + "\" }}\n"
+
+        texture_coordinate = "texCoord TextureCoordinate { point [\n"
+        for point in texture_pts:
+            x = point[0]
+            y = point[1]
+            if x == 0 and y == 0:
+                texture_coordinate += "0 1 ,\n"
+            elif x == 0 and y != 0:
+                texture_coordinate += "0 0 ,\n"
+            elif x != 0 and y == 0:
+                texture_coordinate += "1 0 ,\n"
+            elif x != 0 and y != 0:
+                texture_coordinate += "1 1 ,\n"
+
+        texture_coordinate += "]}\n"
+        end_faceset = "}\n\n"
+
+        indexed_faceset = "geometry IndexedFaceSet { coordIndex [0, 1, 2, 3, -1]\n"
+        end_shape = "}\n\n"
+        self.vrml_file += \
+            shape + \
+            texture + \
+            indexed_faceset + \
+            coordinate3 + \
+            texture_coordinate + \
+            end_faceset + \
+            end_shape
+
+
+        # self.vrml_file += \
+        #     shape + \
+        #     coordinate3 + \
+        #     texture + \
+        #     texture_coordinate + \
+        #     indexed_faceset + \
+        #     end_shape
+
+    def debug(self):
+        texture = []
+        world = []
+
+        for i in range(4):
+            texture.append([i, 0])
+            texture.append([i, 1])
+
+        world.append([0, 0, 0])
+        world.append([100, 0, 0])
+        world.append([0, 100, 0])
+        world.append([100, 100, 0])
+
+        self.startVrml()
+        self.appendPolygon('texture1.jpg', world, texture)
+        self.appendPolygon('texture2.jpg', world, texture)
+        self.appendPolygon('texture3.jpg', world, texture)
+        self.endVrml()
